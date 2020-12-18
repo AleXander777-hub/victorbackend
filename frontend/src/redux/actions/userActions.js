ac4772e3d105f9b8a99175fafa3699811d35d32c
AleXander777-hub <aleksandrpereselkov77@gmail.com>
2020-12-18 18:38:25 +0700

    Auth

--- frontend/src/redux/actions/userActions.js
+++ frontend/src/redux/actions/userActions.js
@@ -1,46 +1,23 @@
 import axios from "axios-jsonp-pro";
+import { toBase64 } from "js-base64";
 import { types } from "./types";
 
 export const LoginUser = (username, password) =>
   async function (dispatch) {
-    console.log(username, password, "Whats");
+    console.log(username, password, "Fuck");
 
-    var instance = axios.create({
-      validateStatus: function (status) {
-        return status == 200;
+    const params = {
+      username: username,
+      password: password,
+    };
+    axios({
+      method: "get",
+      url: "http://export.mysite/api/",
+      params: params,
+      withCredentials: true,
+      headers: {
+        Authorization: "Basic " + toBase64(username + ":" + password),
       },
-    });
-
-    await instance
-      .post("http://export.mysite/api/", {
-        username: username,
-        password: password,
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res, "Hey");
-
-          dispatch({
-            type: types.USER_LOGIN,
-            payload: username,
-          });
-          localStorage.setItem("access_token", res.data.access_token);
-        } else {
-          throw new Error("error");
-        }
-      })
-      .catch((error) => {
-        if (error.response) {
-          alert(
-            "У вас ошибка с кодом" +
-              " " +
-              error.response.status +
-              " " +
-              "в ней написано" +
-              " " +
-              error.response.statusText
-          );
-          console.log(error.response, "FUUUUCK");
-        }
-      });
+    }).then((res) => console.log(res, "WHAT"));
+    console.log("Here");
   };
