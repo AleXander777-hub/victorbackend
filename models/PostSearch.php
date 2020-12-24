da9388b7bbb1c1370cd5dd3570571bc3708b9b22
AleXander777-hub <aleksandrpereselkov77@gmail.com>
2020-12-24 23:00:20 +0700

    Check

--- models/PostSearch.php
+++ models/PostSearch.php
@@ -47,7 +47,7 @@ class PostSearch extends Post
         $dataProvider = new ActiveDataProvider([
             'query' => $query,
             'pagination' => [
-                'pageSize' => 2,
+                'pageSize' => 3,
                 
             ],
             /*'sort' => [
